4e6b3373f7790c5ba90ddb50c916e569309a68bc
praveen.nerkar <praveen.nerkar@experiom.com>
2016-12-26 18:12:32 +0530

    app.js

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
-var builder = require('botbuilder');
-
-var connector = new builder.ConsoleConnector().listen();
-var bot = new builder.UniversalBot(connector);
-var intents = new builder.IntentDialog();
-bot.dialog('/', intents);
-
-intents.matches(/^change name/i, [
-    function (session) {
-        session.beginDialog('/profile');
-    },
-    function (session, results) {
-        session.send('Ok... Changed your name to %s', session.userData.name);
-    }
-]);
-
-intents.onDefault([
-    function (session, args, next) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
-        } else {
-            next();
-        }
-    },
-    function (session, results) {
-        session.send('Hello %s!', session.userData.name);
-    }
-]);
-
-bot.dialog('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT;
+app.get('/', function(req, res){
+ res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', function(socket){
+ socket.on('chat message', function(msg){
+ io.emit('chat message', msg);
+ });
+});
+http.listen(port, function(){
+ console.log('listening on ' + port);
+});
