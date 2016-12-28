27049bce46236a936aa12d4bbaf47d375319684f
praveen.nerkar <praveen.nerkar@experiom.com>
2016-12-28 23:32:39 +0530

    added appID and password

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 var connector = new builder.ChatConnector({
 //    appId: process.env.MICROSOFT_APP_ID,
 //    appPassword: process.env.MICROSOFT_APP_PASSWORD
-    appId: null,
-    appPassword: null
+    appId: 'a3d7b4a7-bdc4-4d72-a6bd-7a99bab69923',
+    appPassword: 'KKqnk5FSZOHmaHwnkUJ4f9C'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
